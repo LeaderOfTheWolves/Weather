66a7f56bca4008aa7f59ab1471a81afdcdd5a3a6
LeaderOfTheWolves <80168240+LeaderOfTheWolves@users.noreply.github.com>
2021-03-07 14:12:01 +0330

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='Weather',
+    version='1.0.0',
+    description='Get Weather Of Your City',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='LeaderOfTheWolves',
+    author_email='ImLeaderOfTheWolves@gmail.com',
+    packages=['weather'],
+    classifiers = [
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
